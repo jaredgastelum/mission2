8a084b5a000df7fcfa06b1acf537077c50df35b6
Jared Gastelum <jaredgastelum@gmail.com>
2023-01-17 17:06:00 -0700

    first commit

---
+++ mission2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace mission2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+// this is a test commit
